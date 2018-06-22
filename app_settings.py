4bc7c5fa2912f6b00d937338f33c817b22a82f69
Jordan Jasik <joj28267@bethel.edu>
2018-06-22 16:02:57 -0500

    Small change to Deleted Sessions Table
    Added the "date deleted" column

--- app_settings.py
+++ app_settings.py
@@ -67,5 +67,10 @@ app_settings = {
         ['Calc 2 Help Session (2/1/2018)', '02/01/2018', '6:55pm - 8:06pm', 'HC 114', 'Tutors', 'Edit/Delete'],
         ['Calc 3 Help Session (2/2/2018)', '02/02/2018', '6:50pm - 8:07pm', 'HC 113', 'Tutored', 'Edit+Delete'],
         ['Calc 4 Help Session (2/3/2018)', '02/03/2018', '6:45pm - 8:09pm', 'HC 11', 'Tutoring', 'Edit-Delete']
+    ],
+    'DELETED_SESSIONS': [
+        ['Calc 2 Help Session (2/1/2018)', '02/01/2018', '02/05/2018', '6:55pm - 8:06pm', 'HC 114', 'Tutors', 'Restore'],
+        ['Calc 3 Help Session (2/2/2018)', '02/02/2018', '02/06/2018', '6:50pm - 8:07pm', 'HC 113', 'Tutored', 'Restored'],
+        ['Calc 4 Help Session (2/3/2018)', '02/03/2018', '02/07/2018', '6:45pm - 8:09pm', 'HC 11', 'Tutoring', 'Restoring']
     ]
 }
