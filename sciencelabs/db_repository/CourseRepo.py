2528fc6652b48d77a71e2f998abd00e0379e8969
Bailey Middendorf <bam95899@bethel.edu>
2018-07-10 13:24:22 -0500

    changed to more relevant method name + formatting

--- sciencelabs/db_repository/CourseRepo.py
+++ sciencelabs/db_repository/CourseRepo.py
@@ -32,7 +32,10 @@ class Course(Base):
                 .filter(User.id == CourseProfessors.professor_id).filter(CourseProfessors.course_id == Course.id)
                 .filter(Course.semester_id == Semester.id).filter(Semester.active == 1).all())
 
-    def get_report_course_info(self):
-        return (session.query(Course.dept, Course.course_num, Course.title, Course.section, User.firstName,
-                              User.lastName)
-                .filter(User.id == CourseProfessors.professor_id).filter(CourseProfessors.course_id == Course.id).filter(Course.semester_id == Semester.id).filter(Semester.active == 1).all())
+    def get_active_course_info(self):
+        return (session.query(Course.dept, Course.course_num, Course.title, Course.section, User.firstName, User.lastName)
+                .filter(User.id == CourseProfessors.professor_id)
+                .filter(CourseProfessors.course_id == Course.id)
+                .filter(Course.semester_id == Semester.id)
+                .filter(Semester.active == 1)
+                .all())
