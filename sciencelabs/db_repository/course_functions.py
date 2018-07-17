5ca9d87ad1ff78617999782cd0664cc0c17ac60e
Bailey Middendorf <bam95899@bethel.edu>
2018-07-17 13:07:07 -0500

    Completed course/admin/(view_course) page

--- sciencelabs/db_repository/course_functions.py
+++ sciencelabs/db_repository/course_functions.py
@@ -6,8 +6,11 @@ class Course:
 
     def get_course_info(self):
         return (session.query(Course_Table, User_Table).filter(Course_Table.num_attendees)
-                .filter(User_Table.id == CourseProfessors_Table.professor_id).filter(CourseProfessors_Table.course_id == Course_Table.id)
-                .filter(Course_Table.semester_id == Semester_Table.id).filter(Semester_Table.active == 1).all())
+                .filter(User_Table.id == CourseProfessors_Table.professor_id)
+                .filter(CourseProfessors_Table.course_id == Course_Table.id)
+                .filter(Course_Table.semester_id == Semester_Table.id)
+                .filter(Semester_Table.active == 1)
+                .all())
 
     def get_active_course_info(self):
         return (session.query(Course_Table, User_Table)
@@ -18,4 +21,21 @@ class Course:
                 .all())
 
     def get_course(self, course_id):
-        return session.query(Course_Table, User_Table).filter(Course_Table.id == course_id).filter(CourseProfessors_Table.course_id == course_id).filter(CourseProfessors_Table.professor_id == User_Table.id).one()
+        return session.query(Course_Table, User_Table, Semester_Table)\
+            .filter(Course_Table.id == course_id)\
+            .filter(CourseProfessors_Table.course_id == course_id)\
+            .filter(CourseProfessors_Table.professor_id == User_Table.id)\
+            .filter(Course_Table.semester_id == Semester_Table.id)\
+            .one()
+        # return session.query(Course_Table, User_Table).filter(Course_Table.id == course_id)\
+        #     .filter(CourseProfessors_Table.course_id == course_id)\
+        #     .filter(CourseProfessors_Table.professor_id == User_Table.id)\
+        #     .one()
+
+    # def get_course_test(self, course_id):
+    #     return session.query(Course_Table, User_Table, Semester_Table)\
+    #         .filter(Course_Table.id == course_id)\
+    #         .filter(CourseProfessors_Table.course_id == course_id)\
+    #         .filter(CourseProfessors_Table.professor_id == User_Table.id)\
+    #         .filter(Course_Table.semester_id == Semester_Table.id)\
+    #         .one()
