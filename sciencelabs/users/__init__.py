98d9372386b2156e78395854ad81fb0f790c7721
Jordan Jasik <joj28267@bethel.edu>
2018-07-25 16:59:40 -0500

    Edit User Updates
    The template was not complete, so I added in the missing fields like role, courses, and the buttons.

--- sciencelabs/users/__init__.py
+++ sciencelabs/users/__init__.py
@@ -5,12 +5,16 @@ from flask_classy import FlaskView, route
 # Local
 from sciencelabs.users.users_controller import UsersController
 from sciencelabs.db_repository.user_functions import User
+from sciencelabs.db_repository.course_functions import Course
+from sciencelabs.db_repository.schedule_functions import Schedule
 
 
 class UsersView(FlaskView):
     def __init__(self):
         self.base = UsersController()
         self.user = User()
+        self.course = Course()
+        self.schedule = Schedule()
 
     def index(self):
         users_info = self.user.get_user_info()
@@ -22,4 +26,8 @@ class UsersView(FlaskView):
 
     def edit_user(self, user_id):
         user = self.user.get_user(user_id)
+        roles = self.user.get_all_roles()
+        user_roles = self.user.get_user_roles(user_id)
+        active_semester = self.schedule.get_active_semester()
+        course_list = self.course.get_semester_courses(active_semester.id)
         return render_template('users/edit_user.html', **locals())
