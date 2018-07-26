ecbe1165c91e85317280e07d7bdb01a8fe362886
Bailey Middendorf <bam95899@bethel.edu>
2018-07-26 09:40:55 -0500

    Removed unused code

--- sciencelabs/__init__.py
+++ sciencelabs/__init__.py
@@ -56,10 +56,8 @@ def datetimeformat(value, custom_format='%l:%M%p'):
         return '???'
 
 
-day_abbr = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 app.jinja_env.filters['datetimeformat'] = datetimeformat
 app.jinja_env.globals.update(app_settings=app_settings)
-app.jinja_env.globals.update(day_abbr=day_abbr)
 
 if __name__ == "__main__":
     app.run()
