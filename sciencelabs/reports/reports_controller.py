d1e9bc79f3d842adffa39fc4809e9303c8140359
Bailey Middendorf <bam95899@bethel.edu>
2018-07-26 15:04:39 -0500

    Removed unused code

--- sciencelabs/reports/reports_controller.py
+++ sciencelabs/reports/reports_controller.py
@@ -1,35 +1,3 @@
 class ReportController:
     def __init__(self):
         super(ReportController, self).__init__
-
-    # TODO FINISH METHOD
-    def get_closed_monthly_info(self):
-        monthly = []
-        for i in range(1, 8):
-            monthly.append([
-                'Schedule Name',
-                'DOW',
-                'Schedule Time',
-                'Total Attendance',
-                '% Total'
-            ])
-        return monthly
-
-    # TODO FINISH METHOD
-    def get_monthly_info(self):
-        monthly = []
-        for i in range(1, 27):
-            monthly.append([
-                'Name',
-                'Date',
-                'DOW',
-                'Schedule Time',
-                'Total Attendance',
-                'report'
-            ])
-        return monthly
-
-    # TODO FINISH METHOD
-    def get_cumulative_info(self):
-        return []
-
