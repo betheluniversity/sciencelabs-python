d54c20f5f50ae227fb95d614f170c98498bde811
Bailey Middendorf <bam95899@bethel.edu>
2018-08-02 09:37:23 -0500

    Another bug fix for reports/monthly

--- sciencelabs/db_repository/schedule_functions.py
+++ sciencelabs/db_repository/schedule_functions.py
@@ -75,7 +75,7 @@ class Schedule:
         return session.query(Schedule_Table).filter(Schedule_Table.id == schedule_id).one()
 
     def get_schedule_from_session(self, session_id):
-        return session.query(Schedule_Table).filter(Session_Table.id == session_id).filter(Session_Table.schedule_id == Schedule_Table.id).one()
+        return session.query(Schedule_Table).filter(Session_Table.id == session_id).filter(Session_Table.schedule_id == Schedule_Table.id).first()
 
     def get_schedule_tutors(self, schedule_id):
         return session.query(User_Table.id, User_Table.firstName, User_Table.lastName, TutorSchedule_Table.lead,
