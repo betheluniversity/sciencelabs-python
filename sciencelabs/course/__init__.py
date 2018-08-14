0381ee8dffae18ed0badbe8bfd83878301edf144
Bailey Middendorf <bam95899@bethel.edu>
2018-08-14 11:56:04 -0500

    styling + testing

--- sciencelabs/course/__init__.py
+++ sciencelabs/course/__init__.py
@@ -1,11 +1,12 @@
 # Packages
-from flask import render_template
+from flask import render_template, request
 from flask_classy import FlaskView, route
 
 # Local
 from sciencelabs.course.course_controller import CourseController
 from sciencelabs.db_repository.course_functions import Course
 from sciencelabs.db_repository.schedule_functions import Schedule
+from sciencelabs.oracle_procs.db_functions import get_course_is_valid
 
 
 class CourseView(FlaskView):
@@ -15,14 +16,37 @@ class CourseView(FlaskView):
         self.base = CourseController()
         self.course = Course()
         self.schedule = Schedule()
+        self.potential_courses = []
 
     @route('/admin/')
     def index(self):
         course_info = self.course.get_course_info()
         semester = self.schedule.get_active_semester()
+
+        results = get_course_is_valid('COS', 105)
+
+        print(results)
+
         return render_template('course/base.html', **locals())
 
     @route('<int:course_id>')
     def view_course(self, course_id):
         course, user, semester = self.course.get_course(course_id)
         return render_template('course/view_course.html', **locals())
+
+    @route("/add-potential-courses", methods=['POST'])
+    def add_potential_course(self):
+        form = request.form
+        sbj_num = form.get('sbj_num')
+        self.potential_courses.append(sbj_num)
+        return ''
+
+    @route("/submit", methods=['POST'])
+    def submit(self):
+        results = []
+        for data in self.potential_courses:
+            result = get_course_is_valid(data[:3], data[3:])
+            if result:
+                results.append(result)
+        self.potential_courses = []
+        return results
