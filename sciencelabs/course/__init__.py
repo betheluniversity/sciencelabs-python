7019ea6441a58a2f044a4d5602986ffaef1b5789
Bailey Middendorf <bam95899@bethel.edu>
2018-08-22 14:22:00 -0500

    string formatting

--- sciencelabs/course/__init__.py
+++ sciencelabs/course/__init__.py
@@ -50,7 +50,7 @@ class CourseView(FlaskView):
         active_coursecodes = self.course.get_active_coursecode()
         cc_str = ''
         for coursecodes in active_coursecodes:
-            cc_str += coursecodes.dept + str(coursecodes.courseNum) + ' (' + coursecodes.courseName + ')' + '; '
+            cc_str += '%s%s (%s); ' % (coursecodes.dept, coursecodes.courseNum, coursecodes.courseName)
         semester = self.schedule.get_active_semester()
 
         return render_template('course/course_table.html', **locals())
