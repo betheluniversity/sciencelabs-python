5da27e91898c40f014d1fb84cc4f267aa764b8a5
Jordan Jasik <joj28267@bethel.edu>
2018-09-06 12:11:54 -0500

    App Settings
    Set up app_settings.py to be able to test roles and permissions locally.

--- app_settings.py
+++ app_settings.py
@@ -1,5 +1,9 @@
 app_settings = {
     'LAB_TITLE':  'Math Lab',
     'DATABASE_KEY': 'mysql://root:jj914.base@localhost/mathlab_real_db',
-    'TEST_USER': 'joj28267'
+    'TEST_USERNAME': 'joj28267',
+    'TEST_EMAIL': 'joj28267@bethel.edu',
+    'TEST_FIRSTNAME': 'Jordan',
+    'TEST_LASTNAME': 'Jasik',
+    'TEST_ROLES': ['Administrator', 'Lead Tutor', 'Tutor', 'Student']
 }
