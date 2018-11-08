e220f1b17cc6bc9c481f4d5bd596c2d8f842b4e7
Bailey Middendorf <bam95899@bethel.edu>
2018-11-08 11:08:05 -0600

    added methods to change roles

--- sciencelabs/profile/__init__.py
+++ sciencelabs/profile/__init__.py
@@ -1,24 +1,28 @@
 # Packages
 from flask import render_template, redirect, url_for, request, session
 from flask_classy import FlaskView, route
+import json
 
 # Local
 from sciencelabs.profile.profile_controller import ProfileController
 from sciencelabs.db_repository.user_functions import User
+from sciencelabs.sciencelabs_controller import ScienceLabsController
 from sciencelabs.alerts.alerts import *
 
 
 class ProfileView(FlaskView):
-    route_base = 'user/edit'
+    route_base = 'user'
 
     def __init__(self):
         self.base = ProfileController()
         self.user = User()
+        self.slc = ScienceLabsController()
 
+    @route('/edit')
     def index(self):
         current_alert = get_alert()
         user = self.user.get_user_by_username(session['USERNAME'])
-        return render_template('profile/home.html', **locals())
+        return render_template('profile/profile.html', **locals())
 
     @route('/save_edits', methods=['post'])
     def save_edits(self):
@@ -34,3 +38,20 @@ class ProfileView(FlaskView):
             set_alert('danger', 'Failed to edit user: ' + str(error))
         return redirect(url_for('ProfileView:index'))
 
+    @route('/view-role')
+    def role_viewer(self):
+        role_list = self.user.get_all_roles()
+        return render_template('profile/role_viewer.html', **locals())
+
+    @route('/change-role', methods=['POST'])
+    def change_role(self):
+        self.slc.check_roles_and_route(['Administrator'])
+        if not session['ADMIN-VIEWER']:
+            role = str(json.loads(request.data).get('chosen-role'))
+            session['ADMIN-VIEWER'] = True
+            session['ADMIN-USERNAME'] = session['USERNAME']
+            session['ADMIN-ROLES'] = session['USER-ROLES']
+            session['USERNAME'] = role
+            session['NAME'] = role
+            session['USER-ROLES'] = role
+        return redirect(url_for('ProfileView:role_viewer'))
