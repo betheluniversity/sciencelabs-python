3d22b48bc6d46f72c6c3e8cbf5a20bcfd742491b
Jordan Jasik <joj28267@bethel.edu>
2018-12-18 10:27:23 -0600

    Update DB Key
    The Password for the database has special characters so we need to encode it in order for the sqlalchemy connector to interpret it.

--- sciencelabs/db_repository/__init__.py
+++ sciencelabs/db_repository/__init__.py
@@ -1,3 +1,5 @@
+import urllib.parse
+
 # Packages
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.session import sessionmaker
@@ -6,7 +8,7 @@ from sqlalchemy import create_engine
 # Local
 from sciencelabs import app
 
-db = create_engine(app.config['DATABASE_KEY'])
+db = create_engine(app.config['DATABASE_KEY'].format(urllib.parse.quote_plus(app.config['DATABASE_PASS'])))
 Base = declarative_base()
 Session = sessionmaker(bind=db)
 session = Session()
