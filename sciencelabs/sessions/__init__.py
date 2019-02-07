5896e191ca127c94f3c77b7f70c1ab4abc5bb9ee
Jordan Jasik <joj28267@bethel.edu>
2019-02-07 12:22:24 -0600

    Test
    Trying something

--- sciencelabs/sessions/__init__.py
+++ sciencelabs/sessions/__init__.py
@@ -554,3 +554,4 @@ class SessionView(FlaskView):
         resp = make_response(redirect(app.config['LOGOUT_URL']))
         resp.set_cookie('MOD_AUTH_CAS_S', '', expires=0)
         resp.set_cookie('MOD_AUTH_CAS', '', expires=0)
+        return resp
