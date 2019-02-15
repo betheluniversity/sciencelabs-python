4d8151fe7eec778558e4b985dbdc89927f83d669
c-schwarze <ces55739@bethel.edu>
2019-02-15 11:32:36 -0600

    commit

--- sciencelabs/__init__.py
+++ sciencelabs/__init__.py
@@ -63,7 +63,7 @@ app.jinja_env.filters['datetimeformat'] = datetimeformat
 
 @app.before_request
 def before_request():
-    if 'assets' in request.path or '/cron/' in request.path or '/checkin/' in request.path or '/student-attendance/' in request.path or '/tutor-attendance/' in request.path:
+    if '/static/' in request.path or '/assets/' in request.path or '/cron/' in request.path or '/checkin/' in request.path or '/student-attendance/' in request.path or '/tutor-attendance/' in request.path:
         flask_session['ALERT'] = None
     else:
         active_semester = Schedule().get_active_semester()
