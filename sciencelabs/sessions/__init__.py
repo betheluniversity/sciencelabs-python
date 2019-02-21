45490d1065fefbaf495f5334102b674e327f74e6
c-schwarze <ces55739@bethel.edu>
2019-02-21 15:45:14 -0600

    revert

--- sciencelabs/sessions/__init__.py
+++ sciencelabs/sessions/__init__.py
@@ -419,16 +419,9 @@ class SessionView(FlaskView):
             self.slc.set_alert('danger', 'Failed to restore session: ' + str(error))
             return redirect(url_for('SessionView:deleted'))
 
-    @route('/checkin/<int:session_id>/<session_hash>', methods=['get'])
+    @route('/checkin/<int:session_id>/<session_hash>/<username>', methods=['get'])
     # @route('/no-cas/checkin/<int:session_id>/<session_hash>/<card_id>', methods=['get', 'post'])
-    def student_sign_in(self, session_id, session_hash):
-        username = flask_session['username']
-        return self._logout_caleb(
-            url_for('SessionView:student_sign_in2', session_id=session_id, session_hash=session_hash, username=username))
-
-    @route('/no-cas/checkin2/<int:session_id>/<session_hash>/<username>', methods=['get'])
-    # @route('/no-cas/checkin/<int:session_id>/<session_hash>/<card_id>', methods=['get', 'post'])
-    def student_sign_in2(self, session_id, session_hash, username):
+    def student_sign_in(self, session_id, session_hash, username):
         flask_session['username'] = username
         semester = self.schedule.get_active_semester()
         # Card id gets passed in as none if not used, otherwise its a 5-digit number
