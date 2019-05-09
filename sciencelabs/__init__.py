77c17e79738af6692a557e64d01e8a97a5cfdfe5
Jordan Jasik <joj28267@bethel.edu>
2019-05-09 11:09:13 -0500

    No debug

--- sciencelabs/__init__.py
+++ sciencelabs/__init__.py
@@ -7,7 +7,6 @@ from raven.contrib.flask import Sentry
 from datetime import datetime
 
 app = Flask(__name__)
-app.debug = True
 app.config.from_object('config')
 app.url_map.strict_slashes = False
 
