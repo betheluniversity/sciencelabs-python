aca72e49f6c6316efdb481f9402ee30121bc0d51
Jordan Jasik <joj28267@bethel.edu>
2019-08-23 14:26:09 -0500

    uncomment sentry

--- sciencelabs/__init__.py
+++ sciencelabs/__init__.py
@@ -15,9 +15,9 @@ from sciencelabs.db_repository import db_session
 from sciencelabs.db_repository.user_functions import User
 from sciencelabs.db_repository.schedule_functions import Schedule
 
-# sentry = Sentry(app, dsn=app.config['SENTRY_URL'], logging=True, level=logging.INFO)
-# if app.config['ENVIRON'] == 'prod':
-#     from sciencelabs import error
+sentry = Sentry(app, dsn=app.config['SENTRY_URL'], logging=True, level=logging.INFO)
+if app.config['ENVIRON'] == 'prod':
+    from sciencelabs import error
 
 from sciencelabs.views import View
 from sciencelabs.cron import CronView
