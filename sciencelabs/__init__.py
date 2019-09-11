dcb67cac3132f50c2dba67f2fda3eb0dde5c550a
Bailey Middendorf <bam95899@bethel.edu>
2019-09-11 13:32:12 -0500

    Added in environment check

--- sciencelabs/__init__.py
+++ sciencelabs/__init__.py
@@ -15,7 +15,7 @@ from sciencelabs.db_repository import db_session
 from sciencelabs.db_repository.user_functions import User
 from sciencelabs.db_repository.schedule_functions import Schedule
 
-if app.config['SENTRY_URL']:
+if app.config['ENVIRON'] == 'prod' and app.config['SENTRY_URL']:
     import sentry_sdk
     from sentry_sdk.integrations.flask import FlaskIntegration
     sentry_sdk.init(dsn="https://441ac324abfe4e58911d03fd02170aac@sentry.io/1374674", integrations=[FlaskIntegration()])
