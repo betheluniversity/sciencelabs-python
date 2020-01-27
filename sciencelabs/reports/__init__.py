9bd0a10553e2aa5c3d5819b060e309fbe0292cf1
Bailey Middendorf <bam95899@bethel.edu>
2020-01-27 13:51:10 -0600

    add check if student doesn't have time in our time out filled

--- sciencelabs/reports/__init__.py
+++ sciencelabs/reports/__init__.py
@@ -848,7 +848,8 @@ class ReportView(FlaskView):
         students = []
         for ss in student_sessions:
             student = self.user.get_user(ss.studentId)
-            total_time = (ss.timeOut - ss.timeIn).total_seconds() / 60
+            if ss.timeOut and ss.timeIn:
+                total_time = (ss.timeOut - ss.timeIn).total_seconds() / 60
             students.extend([{
                 'id': student.id,
                 'firstName': student.firstName,
