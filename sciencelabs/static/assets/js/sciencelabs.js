a843707f47b0bbb2a0e7a41ba5c011161815b69e
Bailey M <bailey-middendorf@bethel.edu>
2020-07-24 13:06:07 -0500

    add show courses js

--- sciencelabs/static/assets/js/sciencelabs.js
+++ sciencelabs/static/assets/js/sciencelabs.js
@@ -1,15 +1,21 @@
 // File for sciencelabs shared JavaScript
 
 $(document).ready(function(){
-
     $('[data-toggle="tooltip"]').tooltip();
 
-   $(".show-tutors").click(function() {
-       $(this).next().toggle();
-       if($(this).text() !== 'Show')
-           $(this).text('Show');
-       else
-           $(this).text('Hide');
+    $(".show-tutors").click(function() {
+        $(this).next().toggle();
+        if($(this).text() !== 'Show')
+            $(this).text('Show');
+        else
+            $(this).text('Hide');
     });
 
+    $(".show-courses").click(function() {
+        $(this).next().toggle();
+        if($(this).text() !== 'Show')
+            $(this).text('Show');
+        else
+            $(this).text('Hide');
+    });
 });
